a0326ec174109cbd0675052dd053b794f80d3313
300stveou <300steveou@gmail.com>
2019-07-31 00:24:32 +0800

    1.新增登入頁

--- ejobs/src/router/index.js
+++ ejobs/src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from "@/components/HelloWorld";
-import Page from "@/components/pages/page";
-import Account from "@/components/pages/account";
-import About from "@/components/pages/about";
+import Login from "@/components/pages/Login";
 
 Vue.use(VueRouter);
 
@@ -13,26 +11,14 @@ export default new VueRouter({
       // 元件呈現的名稱
       name: '首頁',
       // 對應的虛擬路徑
-      path: '/index',
+      path: '/',
       // 對應的元件
       component: Home
     },
     {
-      name: '分頁',
-      path: '/page',
-      component: Page,
-      children: [
-        {
-          name: '分業1',
-          path: '',
-          component: Account
-        },
-        {
-          name: '分頁2',
-          path: 'child/id',
-          component: About
-        },
-      ]
+      path: '/login',
+      name: 'Login',
+      component: Login
     },
   ]
 })
